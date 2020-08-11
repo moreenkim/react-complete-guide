10f73dda272ab0d632bc40687cd16d39fc9fdd59
Moreen <moreenkim36@gmail.com>
2020-08-11 12:52:03 +0300

    Add two way binding

--- src/App.js
+++ src/App.js
@@ -23,6 +23,16 @@ class App extends Component {
     });
   };
 
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: 'max', age: 28 },
+        { name: event.target.value, age: 29 },
+        { name: 'Stephanie', age: 26 },
+      ],
+    });
+  };
+
   render() {
     return (
       <div className="App">
@@ -39,6 +49,7 @@ class App extends Component {
           name={this.state.persons[1].name}
           age={this.state.persons[1].age}
           click={this.switchNameHandler.bind(this, 'max!!')}
+          changed={this.nameChangedHandler}
         >
           {' '}
           My hobbies: Racing
