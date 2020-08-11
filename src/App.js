2ec08a6f7d91c0a3340bdc1ab58fc635cf7e0b1b
Moreen <moreenkim36@gmail.com>
2020-08-11 13:12:01 +0300

    Work with inline styling

--- src/App.js
+++ src/App.js
@@ -34,11 +34,22 @@ class App extends Component {
   };
 
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer',
+    };
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is actually working!</p>
-        <button onClick={() => this.switchNameHandler('maximillian!!')}>
+        <button
+          style={style}
+          onClick={() => this.switchNameHandler('maximillian!!')}
+        >
           Switch Name
         </button>
         <Person
