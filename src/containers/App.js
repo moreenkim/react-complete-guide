ec7bb461fd7c945c836cb9d370b223b76a3dd7d6
Moreen <moreenkim36@gmail.com>
2020-08-14 15:31:23 +0300

    Use contect api

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,6 +6,8 @@ import Cockpit from '../components/Cockpit/Cockpit';
 import Aux from '../hoc/Aux';
 import withClass from '../hoc/withClass';
 
+export const AuthContext = React.createContext(false);
+
 class App extends PureComponent {
   constructor(props) {
     super(props);
@@ -19,6 +21,7 @@ class App extends PureComponent {
       otherState: 'some other value',
       showPersons: false,
       toggleClicked: 0,
+      authenticated: false,
     };
   }
 
@@ -94,6 +97,10 @@ class App extends PureComponent {
     });
   };
 
+  loginHandler = () => {
+    this.setState({ authenticated: true });
+  };
+
   render() {
     console.log('[App.js] Inside render()');
     let persons = null;
@@ -121,9 +128,12 @@ class App extends PureComponent {
           appTitle={this.props.title}
           showPersons={this.state.showPersons}
           persons={this.state.persons}
+          login={this.loginHandler}
           clicked={this.togglePersonsHandler}
         />
-        {persons}
+        <AuthContext.Provider value={this.state.authenticated}>
+          {persons}
+        </AuthContext.Provider>
       </Aux>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
