40f8c7efdbf3b28c118bf1f8c7ba368f830fe4bd
Moreen <moreenkim36@gmail.com>
2020-08-14 14:48:56 +0300

    Validate props

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 //import Radium from 'radium';
 
 import classes from './Person.css';
@@ -35,5 +36,12 @@ class Person extends Component {
     );
   }
 }
+
+Person.PropTypes = {
+  click: PropTypes.func,
+  name: PropTypes.string,
+  age: PropTypes.number,
+  changed: PropTypes.func,
+};
 
 export default withClass(Person, classes.Person); //Radium(person);
